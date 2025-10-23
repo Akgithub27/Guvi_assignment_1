e3865d574bea0d3f2a42154a492eba759d79444b
AakashPrakash <aakashprakash27@gmail.com>
2025-10-23 21:51:37 +0530

    program number 1

---
+++ LoopingExample.java
@@ -0,0 +1,15 @@
+public class LoopingExample {
+    public static void main(String[] args) {
+        //using for loop
+        for(int i=10;i<=50;i++){
+            System.out.println(i);
+        }
+
+        //while loop
+        int i=10;
+        while(i<=50){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
