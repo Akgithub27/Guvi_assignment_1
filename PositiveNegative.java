116e6716453a90f255b512e476509d0c74d0cbdb
AakashPrakash <aakashprakash27@gmail.com>
2025-10-23 22:07:14 +0530

    program number 2 and 3

---
+++ PositiveNegative.java
@@ -0,0 +1,10 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int number =123;
+        if(number>0){
+            System.out.println("it is Positive");
+        }else {
+            System.out.println("It is negative");
+        }
+    }
+}
